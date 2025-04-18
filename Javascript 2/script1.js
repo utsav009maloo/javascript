640aad70e07f791454b4bc231f6818206e560724
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-04-18 11:51:48 +0530

    Map Function

--- Javascript 2/script1.js
+++ Javascript 2/script1.js
@@ -10,8 +10,17 @@ const work = ['Plumber','fireman','police','doctor']
 // work.pop('police')
 // console.log(work)
 
-// FOR EACH Function me you can make a functions and call them
-function how(n){
-    console.log(n)
-}
-work.forEach(how)
+// FOR EACH Function me you can make a functions and call them. make loop but not make new array
+// function how(n){
+//     console.log(n)
+// }
+// work.forEach(how)
+
+
+//MAP functon it make new loop and also new array
+// const num = [1,2,3,4,5,6,7,8]//Make array
+// function double(n){//Make a function
+//     return n * 2
+// }
+// let newnum = num.map(double)//create new array and call old array and call function
+// console.log(newnum)//print new array
