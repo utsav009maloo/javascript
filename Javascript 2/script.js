5704c31698f15a2af35863b32b423bec3fc63ec9
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-04-18 11:40:59 +0530

    Array work

--- Javascript 2/script.js
+++ Javascript 2/script.js
@@ -11,9 +11,9 @@
 // }
 // console.log(sub(6 , 2))
 
-//2.argument Keywords
+// 2.argument Keywords
 
-const addnum = (...num) => {
-console.log(num)
-}
-addnum(5,5,5,4,6,8)
+// const addnum = (...num) => {
+// console.log(num)
+// }
+// addnum(5,5,5,4,6,8)
--- Javascript 2/script1.js
+++ Javascript 2/script1.js
@@ -1,1 +1,17 @@
 //Array
+const work = ['Plumber','fireman','police','doctor']
+// console.log(work)
+// console.log(work.length)
+// console.log(work[2])
+
+// work.push('rony')
+// console.log(work)
+
+// work.pop('police')
+// console.log(work)
+
+// FOR EACH Function me you can make a functions and call them
+function how(n){
+    console.log(n)
+}
+work.forEach(how)
