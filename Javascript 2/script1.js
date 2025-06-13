6447f907bd10ccaa19fd403e8c7c3793e5aa90e4
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-06-13 14:08:14 +0530

    .

--- Javascript 2/script1.js
+++ Javascript 2/script1.js
@@ -1,10 +1,13 @@
 //Array
-const work = ['Plumber','fireman','police','doctor']
+// const work = ['Plumber','fireman','police','doctor']
 // console.log(work)
 // console.log(work.length)
 // console.log(work[2])
 
 // work.push('rony')
+// console.log(work)
+
+// work.unshift('Hero')//He add value in first poistion of array.
 // console.log(work)
 
 // work.pop('police')
@@ -29,3 +32,21 @@ const work = ['Plumber','fireman','police','doctor']
 // const num = [1,2,3,4,5,6,7,8]
 // let ans = num.find((num) => num === 7)
 // console.log(ans)
+
+
+
+//concat method used for merg to array
+// let num = [10,30,20,40,50,70]
+// let num2 = [100,300,200,400,500,700]
+// console.log(num.concat(num2))
+// console.log(num.reverse())
+// console.log(num.sort())
+// console.log(num.slice(1,3))
+
+// //Spread Operator
+// let num = [101,"Johny",2025]
+// let num1 = [...num]
+// num1.push("Why")
+// console.log(num);
+// console.log(num1);
+
