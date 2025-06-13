29a2d553caa9536e4c78d10e66e4b39b57606d2c
utsav009maloo <72725183+utsav009maloo@users.noreply.github.com>
2025-06-13 16:49:09 +0530

    Javascript

---
+++ Javascript 2/script2.js
@@ -0,0 +1,27 @@
+//Object in Array
+// let arr = [
+//     {user:1, name:"Utsav"},
+//     {user:2, name:"Maloo"},
+//     {user:3, name:"God"},
+//     {user:4, name:"Hero"}
+// ]
+// console.log(arr);
+
+//For in function
+// for(let i in arr){
+//     console.log(arr[i]);   
+// }
+
+
+// //Destructive 
+// let [item1,item2] = arr;
+// console.log(item1);
+// console.log(item2); 
+
+// //MAP
+// var map1 = new Map([[1,"one"],
+//     ["Name","Utsav"],
+//     ["whole no.",[10,20,30,40,50,60]]]
+// )
+// console.log(map1);
+
